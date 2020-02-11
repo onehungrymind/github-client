deed738f46036282f636d1e6487cb06d3c61727f
elideg <58085156+elideg@users.noreply.github.com>
2020-02-11 11:55:38 -0700

    [VA-4] toolbar (#10)
    * added toolbar
    
    * fixed styling on toolbar, added bootstrap utilities, removed cdk from material bootstrap module
    
    * Apply suggestions from code review
    
    Co-Authored-By: Victor Avila <31123803+VicAv99@users.noreply.github.com>
    
    * changed formating, added bootstrap-4-grid
    
    * removed default font styles, formatted scss files
    
    * Update libs/ui-libraries/src/lib/ui-toolbar/ui-toolbar.component.scss
    
    Co-authored-by: Victor Avila <31123803+VicAv99@users.noreply.github.com>

--- libs/ui-libraries/src/lib/ui-toolbar/ui-toolbar.component.ts
+++ libs/ui-libraries/src/lib/ui-toolbar/ui-toolbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'vulnerability-checker-ui-toolbar',
+  selector: 'gc-ui-toolbar',
   templateUrl: './ui-toolbar.component.html',
-  styleUrls: ['./ui-toolbar.component.scss']
+  styleUrls: ['./ui-toolbar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UiToolbarComponent implements OnInit {
 
