38c73cf71579f7527e73830fb9a60dcbc50fff1d
Elijah de Gouveia <elijah@venturplex.com>
2020-02-10 17:54:35 -0700

    added toolbar

--- libs/ui-libraries/src/lib/ui-libraries.module.ts
+++ libs/ui-libraries/src/lib/ui-libraries.module.ts
@@ -1,9 +1,14 @@
+import { MaterialModule } from '@gc/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiToolbarComponent } from './ui-toolbar/ui-toolbar.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [UiToolbarComponent]
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [UiToolbarComponent],
+  exports: [UiToolbarComponent]
 })
 export class UiLibrariesModule {}
