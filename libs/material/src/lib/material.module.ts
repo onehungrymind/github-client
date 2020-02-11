18eee91520d46d6049c4366ca04c3ba0d40ac1cd
Elijah de Gouveia <elijah@venturplex.com>
2020-02-11 10:26:14 -0700

    fixed styling on toolbar, added bootstrap utilities, removed cdk from material bootstrap module

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -37,34 +37,11 @@ import {
   MatTooltipModule,
   MatTreeModule,
 } from '@angular/material';
-import {A11yModule} from '@angular/cdk/a11y';
-import {BidiModule} from '@angular/cdk/bidi';
-import {ObserversModule} from '@angular/cdk/observers';
-import {OverlayModule} from '@angular/cdk/overlay';
-import {PlatformModule} from '@angular/cdk/platform';
-import {PortalModule} from '@angular/cdk/portal';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-
 /**
  * NgModule that includes all Material modules.
 */
 @NgModule({
   exports: [
-    // CDK
-    A11yModule,
-    BidiModule,
-    ObserversModule,
-    OverlayModule,
-    PlatformModule,
-    PortalModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,
-    // Material
     MatAutocompleteModule,
     MatBadgeModule,
     MatBottomSheetModule,
