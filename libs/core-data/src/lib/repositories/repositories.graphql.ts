33986cd7266e1890b11ef1771d9d931c693f763e
david <david@venturplex.com>
2020-02-11 12:36:50 -0700

    incr changes

--- libs/core-data/src/lib/repositories/repositories.graphql.ts
+++ libs/core-data/src/lib/repositories/repositories.graphql.ts
@@ -1,9 +1,38 @@
 import gql from 'graphql-tag';
-  
-export const repositoriesFragment = gql`
-  fragment repoFragment on repositories {
-    id
-    name
-    positiom
+
+export interface RepositoriesType {
+  allrepos: Repository[];
+  notfixed: Repository[];
+}
+
+export interface Repository {
+  name: string,
+  url: string,
+    vulnerabilityAlerts: {
+      totalCount: number
   }
-`;
+}
+
+export const vulFragment = gql`
+  fragment vulFragment on Repository {
+    name
+    url
+    vulnerabilityAlerts(first: 100) {
+      totalCount
+    }
+  }
+`
+
+export const vulQuery = gql`
+  query vulQuery {
+  organization(login: "onehungrymind") {
+    repositories(last: 100) {
+      totalCount
+      nodes {
+        ...vulFragment
+      }
+    }
+  }
+}
+${vulFragment}
+`
--- libs/core-data/src/lib/repositories/repositories.service.ts
+++ libs/core-data/src/lib/repositories/repositories.service.ts
@@ -1,9 +1,31 @@
+import { vulQuery } from './repositories.graphql';
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { RepositoriesType } from './repositories.graphql';
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { ApolloQueryResult } from 'apollo-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoriesService {
+  public vulnerabilities: RepositoriesType[];
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo
+  ) { }
+
+  all(): Observable<any> {
+    return this.apollo.query({
+      query: vulQuery,
+      fetchPolicy: 'network-only'
+    })
+      .pipe(
+        map(
+          (res: ApolloQueryResult<any>) =>
+            res.data.organization.repositories.nodes
+        )
+      )
+  }
 }
