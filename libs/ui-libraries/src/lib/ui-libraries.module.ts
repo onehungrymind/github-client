99ae39fad97edc93337a48216cd2d128ad80ea50
david <david@venturplex.com>
2020-02-12 09:41:54 -0700

    incr chanhes

--- libs/ui-libraries/src/lib/ui-libraries.module.ts
+++ libs/ui-libraries/src/lib/ui-libraries.module.ts
@@ -2,13 +2,14 @@ import { MaterialModule } from '@gc/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiToolbarComponent } from './ui-toolbar/ui-toolbar.component';
+import { UiLoginComponent } from './ui-login/ui-login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [UiToolbarComponent],
+  declarations: [UiToolbarComponent, UiLoginComponent],
   exports: [UiToolbarComponent]
 })
 export class UiLibrariesModule {}
---
+++ libs/ui-libraries/src/lib/ui-login/ui-login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+// import { githubConfig } from '../../../../../.env';
+
+@Component({
+  selector: 'vulnerability-checker-ui-login',
+  templateUrl: './ui-login.component.html',
+  styleUrls: ['./ui-login.component.scss']
+})
+export class UiLoginComponent {
+
+  loginWithGitHub() {
+    // window.location.href = `https://github.com/login/oauth/authorize?client_id=${githubConfig.client_id}&scope=user,public_repo,repo&redirect_uri=${window.location.href}/callback`;
+  }
+}
