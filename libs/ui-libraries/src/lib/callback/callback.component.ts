ee969ea872242ee7c44386dde90d5666f9270fdb
Elijah de Gouveia <elijah@venturplex.com>
2020-02-12 08:33:41 -0700

    generated callback component

---
+++ libs/ui-libraries/src/lib/callback/callback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gc-callback',
+  templateUrl: '',
+  styleUrls: [''']
+})
+export class CallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/ui-libraries/src/lib/ui-libraries.module.ts
+++ libs/ui-libraries/src/lib/ui-libraries.module.ts
@@ -2,13 +2,14 @@ import { MaterialModule } from '@gc/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiToolbarComponent } from './ui-toolbar/ui-toolbar.component';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [UiToolbarComponent],
+  declarations: [UiToolbarComponent, CallbackComponent],
   exports: [UiToolbarComponent]
 })
 export class UiLibrariesModule {}
