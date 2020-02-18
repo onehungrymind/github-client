57d119d5be9ae9a1ef9b6017bdd851aa00ca0b47
elideg <58085156+elideg@users.noreply.github.com>
2020-02-18 09:55:59 -0700

    [VA-8] info (#16)
    * created repo-info component
    
    * refactored css in repo-info, and ui-toolbar
    
    * cleaned up styling --david
    
    * last chnage. changed order of css classes
    
    * fix styles accordingly to PR
    
    * fixed styling on toolbar and repo info
    
    Co-authored-by: David Avila <david@venturplex.com>

--- libs/ui-libraries/src/index.ts
+++ libs/ui-libraries/src/index.ts
@@ -1,2 +1,3 @@
+export { RepoInfoComponent } from './lib/repo-info/repo-info.component';
 export { UiToolbarComponent } from './lib/ui-toolbar/ui-toolbar.component';
 export { UiLibrariesModule } from './lib/ui-libraries.module';
--- libs/ui-libraries/src/lib/ui-libraries.module.ts
+++ libs/ui-libraries/src/lib/ui-libraries.module.ts
@@ -2,13 +2,14 @@ import { MaterialModule } from '@gc/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiToolbarComponent } from './ui-toolbar/ui-toolbar.component';
+import { RepoInfoComponent } from './repo-info/repo-info.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [UiToolbarComponent],
-  exports: [UiToolbarComponent]
+  declarations: [UiToolbarComponent, RepoInfoComponent],
+  exports: [UiToolbarComponent, RepoInfoComponent]
 })
 export class UiLibrariesModule {}
